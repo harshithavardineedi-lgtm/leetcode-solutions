6d300544d969bfdf4c7f3df89464e248c8fbaa53
Harshitha Vardhineedi <harshithavardineedi@gmail.com>
2026-09-26 20:12:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (27.22%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+
+        negative = (dividend < 0) != (divisor < 0)
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+
+        while dividend >= divisor:
+            value = divisor
+            multiple = 1
+
+            while dividend >= value + value:
+                value += value
+                multiple += multiple
+
+            dividend -= value
+            result += multiple
+
+        if negative:
+            result = -result
+
+        return result    
